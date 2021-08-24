b238d7c2655be29cb94de17d94e77be795bf3862
Jungwoo <dave.lee@uniquedonut.com>
2021-08-24 11:40:07 +0900

    separate schema-reg to find and register

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {res.render('home', { title: '홈'});});
 router.get('/signin', function(req, res, next) {res.render('signin', { title: '로그인'});});
 router.get('/manager', function(req, res, next) {res.render('manager', { title: '스키마 관리' });});
+router.get('/register', function(req, res, next) {res.render('register', { title: '스키마 관리' });});
 router.get('/analyzer', function(req, res, next) {res.render('analyzer', { title: '다양성 검사' });});
 router.get('/result', function(req, res, next) {res.render('result', { title: '정확성 검사' });});
 router.get('/report', function(req, res, next) {res.render('report', { title: '레포트 출력' });});
